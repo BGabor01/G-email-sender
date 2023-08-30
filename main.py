55f0880068ab97b75b4d36d5ec0e4a8f9164fa75
Balogh Gabor Jozsef <busnes14@gmail.com>
2023-08-30 23:03:31 +0200

    main.py: Using EmailSender class

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from g_rpc import Server
-from rpc_commands import send_reg_email
+import os
+import logging
+from utils import EmailSender
+from dotenv import load_dotenv
+import time
 
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
-    server = Server("EMAIL_SENDER")
+    
+    logger = logging.getLogger("MAIN")
+
+    email_sender = EmailSender(os.environ.get("SENDER_EMAIL"),
+                               os.environ.get("PASSWORD"),
+                               os.environ.get("SERVER_HOST"),
+                               os.environ.get("SERVER_PORT"))
+    
+    logger.info("initalized EmailSender")
+    
+    server = Server("EMAIL_SENDER", host = os.environ.get("RABBITMQ_HOST"))
     server.connect()
-    server.add_method('registration_email', send_reg_email)
+    server.add_method('order_completed', email_sender.order_completed)
+    logger.info("Order_completed method done")
     server.start()
